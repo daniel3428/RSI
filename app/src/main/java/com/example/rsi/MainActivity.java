bbf40c414ec926ad2a16cba1377f0e221a49fbfd
daniel3428 <daniel153282028@gmail.com>
2021-03-21 22:26:40 +0800

    add Button event

--- app/src/main/java/com/example/rsi/MainActivity.java
+++ app/src/main/java/com/example/rsi/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.rsi;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     ViewPager pager;
     ArrayList<View> pagerList;
+    private Button getBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,27 @@ public class MainActivity extends AppCompatActivity {
         pagerList.add(v1);
         pager.setAdapter(new MyViewPagerAdapter(pagerList));
         pager.setCurrentItem(0);
+
+        getBtn = (Button) v1.findViewById(R.id.http_get_btn);
+        getBtn.setOnClickListener(btn1Listener);
     }
 
+    private Button.OnClickListener btn1Listener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.http_get_btn:
+                    //Log.i("123","321");
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    };
+
+    //依照按下的按鈕去做相對應的任務
+    public void onClick(View v){
+    }
 
 }
